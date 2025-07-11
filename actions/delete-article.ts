2f8dd146e99dd58598ae54e3604e1c4cfece5f58
Aeyan-Raza-pk <www.aeyanraza65@gmail.com>
2025-07-11 19:12:28 +0500

    code update

--- actions/delete-article.ts
+++ actions/delete-article.ts
@@ -4,6 +4,12 @@ import { Prisma } from "@/lib/prisma";
 import { revalidatePath } from "next/cache";
 
 export const deleteArticle = async (articleId: string) => {
+    await Prisma.comment.deleteMany({
+    where: { articleId },
+  });
+    await Prisma.like.deleteMany({
+    where: { articleId },
+  });
   await Prisma.article.delete({
     where: {
       id: articleId,
