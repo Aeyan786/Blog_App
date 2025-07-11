6f797e66964d845b8fb7df8cd819155fce14fa96
Aeyan-Raza-pk <www.aeyanraza65@gmail.com>
2025-07-12 01:47:25 +0500

    eslint configure

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -24,6 +24,7 @@ const eslintConfig = [
       "@typescript-eslint/no-require-imports": "off",
       "@typescript-eslint/no-unnecessary-type-constraint": "off",
       "@typescript-eslint/no-wrapper-object-types": "off",
+      "@typescript-eslint/no-explicit-any": "off",
     },
   },
 ];
